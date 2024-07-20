e98525cec53c7ecbc70ca24a243e88239f24cccd
VaibhavJagtap9 <jagtapvaibhav507@gmail.com>
2024-07-20 12:06:09 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,25 @@
+import Feed from "@/components/Feed" //import client component
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powred Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting 
+        tool for modern world to discover, create 
+        and share creative prompts
+      </p>
+
+      <Feed/>
+    </section>
+
   )
 }
 
 export default Home
+
+
---
+++ components/Feed.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
